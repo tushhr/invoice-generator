57ee9b214cec691f76785d6dd133b28c1db3fcc8
Tushhr <tusharmaheshwaritm6@gmail.com>
2023-09-26 18:11:27 +0530

    feat: add constant file

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,88 +4,50 @@ import Button from 'react-bootstrap/Button';
 
 import { removeInvoice, clearInvoiceList } from "../store/invoiceStore";
 import { clearLocalStorage } from "../utils/localStorage";
-import { INVOICE_ACTIONS } from "../utils/constant";
+import { 
+    INVOICE_ACTIONS, 
+    INVOICE_TABLE_HEADERS, 
+    TABLE_HEADER_TO_INVOICE_KEY, 
+    INT_FIELD_COLOUMNS, 
+    STRING_FIELD_COLOUMNS, 
+    DATE_FIELD_COLOUMNS 
+} from "../utils/constant";
 import InvoiceForm from "./InvoiceForm";
 
 export default function Home() {
     const invoiceListStore = useSelector(store => store.invoice.invoiceList)
+    const dispatch = useDispatch()
 
     const [invoiceList, setInvoiceList] = useState(invoiceListStore)
     const [invoice, setInvoice] = useState(null)
     const [invoiceAction, setInvoiceAction] = useState(INVOICE_ACTIONS.NEW_INVOICE)
-    const [tableHeader, setTableHeader] = useState([
-        {
-            title: "Invoice",
-            icon: "fa-chevron-down",
-        },
-        {
-            title: "From",
-            icon: "fa-chevron-down",
-        },
-        {
-            title: "To",
-            icon: "fa-chevron-down",
-        },
-        {
-            title: "Date",
-            icon: "fa-chevron-down",
-        },
-        {
-            title: "Total Amount",
-            icon: "fa-chevron-down",
-        },
-        {
-            title: "",
-            icon: "",
-        },
-        {
-            title: "",
-            icon: "",
-        },
-        {
-            title: "",
-            icon: "",
-        }
-    ])
+    const [tableHeader, setTableHeader] = useState(INVOICE_TABLE_HEADERS)
 
-    const nameToKey = {
-        'invoice': 'invoiceNumber',
-        'from': 'billFrom',
-        'to': 'billTo',
-        'date': 'dateOfIssue',
-        'total amount': 'total',
+    const nameToKey = TABLE_HEADER_TO_INVOICE_KEY
+
+    const sortByInt = (array, key) => {
+        return [...array].sort((a, b) =>  a.info[key] - b.info[key]);
     }
 
-    const dispatch = useDispatch()
-
-    const sortByInt = (array, name) => {
-        const key = nameToKey[name]
-        const newInvoiceList = [...array].sort((a, b) =>  a.info[key] - b.info[key]);
-        return newInvoiceList
+    const sortByString = (array, key) => {
+        return [...array].sort((a, b) => a.info[key].localeCompare(b.info[key]));;
     }
 
-    const sortByString = (array, name) => {
-        const key = nameToKey[name]
-        const newInvoiceList = [...array].sort((a, b) => a.info[key].localeCompare(b.info[key]));
-        return newInvoiceList;
-    }
-
-    const sortByDate = (array, name) => {
-        const key = nameToKey[name]
-        const newInvoiceList = [...array].sort((a, b) => new Date(a.info[key]) - new Date(b.info[key]));
-        return newInvoiceList
+    const sortByDate = (array, key) => {
+        return [...array].sort((a, b) => new Date(a.info[key]) - new Date(b.info[key]));
     }
 
     const handleSorting = (index, sortOrder) => {
         const colHeader = tableHeader[index].title.toLowerCase()
+        const key = nameToKey[colHeader]
         let newInvoiceList = invoiceList;
 
-        if(colHeader === "invoice" || colHeader === "total amount") {
-            newInvoiceList = sortByInt(invoiceList, colHeader)
-        } else if(colHeader === "from" || colHeader === "to") {
-            newInvoiceList = sortByString(invoiceList, colHeader)
-        } else if(colHeader === "date") {
-            newInvoiceList = sortByDate(invoiceList, colHeader)
+        if(colHeader in INT_FIELD_COLOUMNS) {
+            newInvoiceList = sortByInt(invoiceList, key)
+        } else if(colHeader in STRING_FIELD_COLOUMNS) {
+            newInvoiceList = sortByString(invoiceList, key)
+        } else if(colHeader in DATE_FIELD_COLOUMNS) {
+            newInvoiceList = sortByDate(invoiceList, key)
         }
 
         sortOrder === 1 ? setInvoiceList(newInvoiceList) : setInvoiceList(newInvoiceList.reverse());
@@ -131,36 +93,37 @@ export default function Home() {
             { !invoice && 
                 <>
                 {
-                    invoiceList.length ? <table className="invoice-table">
-                <thead>
-                    <tr>
-                        {tableHeader.map((header, index) => 
-                            <th className="invoice-table__col-head invoice-table__col-head--left-align">
-                                {header.title}
-                                <i className={"fa " + header.icon} onClick={() => handleSortingClick(index)}></i>
-                            </th>
-                        )}
-                    </tr>
-                </thead>
-                <tbody>
-                    {invoiceList.map((invoice, index) => {
-                        return (
-                            <tr key={invoice.id}>
-                                <td className="invoice-table__row invoice-table__row--active">{invoice.info.invoiceNumber}</td>
-                                <td className="invoice-table__row"> {invoice.info.billFrom}</td>
-                                <td className="invoice-table__row">{invoice.info.billTo}</td>
-                                <td className="invoice-table__row">{invoice.info.dateOfIssue}</td>
-                                <td className="invoice-table__row">{invoice.currency + invoice.info.total}</td>
-                                <td className="invoice-table__row" onClick={() =>  handleCopy(invoice)}><Button variant="primary" type="submit" className="d-block w-100">Copy</Button></td>
-                                <td className="invoice-table__row" onClick={() =>  { setInvoice(invoice); setInvoiceAction(INVOICE_ACTIONS.EDIT_INVOICE)}}><Button variant="primary" type="submit" className="d-block w-100">Edit</Button></td>
-                                <td className="invoice-table__row" onClick={() =>  dispatch(removeInvoice(invoice.id))}><Button variant="primary" type="submit" className="d-block w-100">Delete</Button></td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-                    </table>
+                    invoiceList.length ? 
+                        <table className="invoice-table">
+                            <thead>
+                                <tr>
+                                    {tableHeader.map((header, index) => 
+                                        <th className="invoice-table__col-head invoice-table__col-head--left-align">
+                                            {header.title}
+                                            <i className={"fa " + header.icon} onClick={() => handleSortingClick(index)}></i>
+                                        </th>
+                                    )}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {invoiceList.map((invoice, index) => {
+                                    return (
+                                        <tr key={invoice.id}>
+                                            <td className="invoice-table__row invoice-table__row--active">{invoice.info.invoiceNumber}</td>
+                                            <td className="invoice-table__row"> {invoice.info.billFrom}</td>
+                                            <td className="invoice-table__row">{invoice.info.billTo}</td>
+                                            <td className="invoice-table__row">{invoice.info.dateOfIssue}</td>
+                                            <td className="invoice-table__row">{invoice.currency + invoice.info.total}</td>
+                                            <td className="invoice-table__row" onClick={() =>  handleCopy(invoice)}><Button variant="primary" type="submit" className="d-block w-100">Copy</Button></td>
+                                            <td className="invoice-table__row" onClick={() =>  { setInvoice(invoice); setInvoiceAction(INVOICE_ACTIONS.EDIT_INVOICE)}}><Button variant="primary" type="submit" className="d-block w-100">Edit</Button></td>
+                                            <td className="invoice-table__row" onClick={() =>  dispatch(removeInvoice(invoice.id))}><Button variant="primary" type="submit" className="d-block w-100">Delete</Button></td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </table>
                     :
-                    <div className="no-invoice invoice-table">No Invoice Found</div>
+                        <div className="no-invoice invoice-table">No Invoice Found</div>
                 }
                 </>
             }
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,3 +3,50 @@ export const INVOICE_ACTIONS = {
     EDIT_INVOICE: 'EDIT_INVOICE',
     COPY_INVOICE: 'COPY_INVOICE',
 }
+
+export const INVOICE_TABLE_HEADERS = [
+    {
+        title: "Invoice",
+        icon: "fa-chevron-down",
+    },
+    {
+        title: "From",
+        icon: "fa-chevron-down",
+    },
+    {
+        title: "To",
+        icon: "fa-chevron-down",
+    },
+    {
+        title: "Date",
+        icon: "fa-chevron-down",
+    },
+    {
+        title: "Total Amount",
+        icon: "fa-chevron-down",
+    },
+    {
+        title: "",
+        icon: "",
+    },
+    {
+        title: "",
+        icon: "",
+    },
+    {
+        title: "",
+        icon: "",
+    }
+]
+
+export const TABLE_HEADER_TO_INVOICE_KEY = {
+    'invoice': 'invoiceNumber',
+    'from': 'billFrom',
+    'to': 'billTo',
+    'date': 'dateOfIssue',
+    'total amount': 'total',
+}
+
+export const INT_FIELD_COLOUMNS = ['invoice', 'total amount']
+export const STRING_FIELD_COLOUMNS = ['from', 'to']
+export const DATE_FIELD_COLOUMNS = ['date']
